c086ed3bd40b8e1e231c8b84546305939211438f
judamov <judamov.09@gmail.com>
2025-08-28 14:47:42 -0500

    feat(semesters): new commands for semesters

--- bot/index.js
+++ bot/index.js
@@ -1,19 +1,26 @@
 import { Telegraf } from "telegraf";
 import { loadUsers } from "./userStore.js";
-import { registerUserCommands } from "./modules/users.js";
-import { registerAcademicCommands } from "./modules/academic.js";
-import { registerSystemCommands } from "./modules/system.js";
+import { registerUserCommands, userCommands } from "./modules/users.js";
+import { registerAcademicCommands, academicCommands } from "./modules/academic.js";
+import { registerSystemCommands, systemCommands } from "./modules/system.js";
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 let data = loadUsers();
 
-// Pasamos el bot y data a cada módulo
+// Registrar comandos de cada módulo
 registerUserCommands(bot, data);
 registerAcademicCommands(bot);
 registerSystemCommands(bot);
 
+// Unir los comandos para el menú de Telegram
+bot.telegram.setMyCommands([
+  ...userCommands,
+  ...academicCommands,
+  ...systemCommands
+]);
+
 // ------------------ Inicio ------------------ //
 bot.catch((err) => console.error("Bot error:", err));
 bot.launch().then(() =>
-  console.log("✅ Bot SIGHA corriendo con módulos separados...")
+  console.log("✅ Bot SIGHA corriendo con módulos y comandos separados...")
 );
